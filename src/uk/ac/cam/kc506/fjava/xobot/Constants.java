e0c56879f03820247e1f765b6f8da0d27f535f10
Keith Collister <hnefatl@users.noreply.github.com>
2017-10-22 21:23:49 +0100

    Fixed typo in Constants.java

--- src/uk/ac/cam/kc506/fjava/xobot/Constants.java
+++ src/uk/ac/cam/kc506/fjava/xobot/Constants.java
@@ -5,7 +5,6 @@ public class Constants
     public static final String BOT_INVOKE = "xo";
     public static final String START_COMMAND = "start";
     public static final String STOP_COMMAND = "stop";
-    public static final String PLAY_COMMAND = "stop";
     public static final String HELP_COMMAND = "help";
 
     public static final String UNKNOWN_COMMAND = "Unknown command. Use \"" + BOT_INVOKE + " " + HELP_COMMAND + "\" for help.";
